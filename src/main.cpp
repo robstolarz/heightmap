537ccd451ceaf323cb431e15ebdef3a0076bd93d
Rob Stolarz <robert.stolarz@yandex.com>
2018-01-05 14:48:20 -0500

    Replace glfw w glad & SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
 #include <stdio.h>
-#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
+#include "glad/glad.h"
 
-int main() {
-    glfwInit();
-    return 0;
+int main(int, char*[]) {
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window* window = SDL_CreateWindow("OGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+
+  gladLoadGLLoader(SDL_GL_GetProcAddress);
+
+  bool running = true;
+  while(running) {
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+      if(event.type == SDL_QUIT)
+        running = false;
+    }
+
+    glClearColor(1, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(window);
+  }
+
+  SDL_GL_DeleteContext(context);
+  SDL_DestroyWindow(window);
+
+  return 0;
 }
